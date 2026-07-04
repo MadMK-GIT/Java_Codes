209acf3ba3fe5580e628917c311f73bbf17edd1c
MADHAN KUMAR P <madhan.p.kumar05@gmail.com>
2026-07-04 11:57:08 +0530

    Selection Sort Code

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays; //SelectionSort
+class SelectionSort 
+{
+	public static void main(String[] args) 
+	{
+		int arr[]={5,3,9,2,6,7};
+		for(int i=0;i<arr.length-1;i++){
+			int min=i;
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[min]>arr[j]){
+					min=j;
+				}
+			}
+			int temp=arr[i];
+			arr[i]=arr[min];
+			arr[min]=temp;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
