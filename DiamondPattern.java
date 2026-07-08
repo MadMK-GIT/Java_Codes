4c5417438cc7428da88c775b3acffb3cb3a7d3e3
MADHAN KUMAR P <madhan.p.kumar05@gmail.com>
2026-07-08 15:57:00 +0530

    pattern 1 done

---
+++ DiamondPattern.java
@@ -0,0 +1,22 @@
+
+public class DiamondPattern
+{
+	public static void main(String[] args) {
+	    int row=5;
+	    int space=4;
+	    int star=1;
+	    for(int i=1;i<=row;i++){
+	        
+	        for(int j=1;j<=space;j++){
+	            System.out.print(" ");
+	        }
+	            for(int k=1;k<=star;k++){
+	            System.out.print("*");
+	            }
+	        System.out.println();
+	        star+=2;
+	        space--;
+	        
+	    }
+	}
+}
